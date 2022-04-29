237b2ded107a6f016d93f654e5cebe251c5e0939
andylin9611052 <andylin9611052@gmail.com>
2022-04-29 16:03:02 +0800

    exzming2

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -3,9 +3,12 @@ package com.fju.order;
 public class Order {
     int amount;
     Delivery delivery;
-
+    int shipFeed = delivery.price;
     public Order(int amount, Delivery delivery) {
         this.amount = amount;
         this.delivery = delivery;
+    }
+    public int total() {
+        return (amount + shipFeed);
     }
 }
